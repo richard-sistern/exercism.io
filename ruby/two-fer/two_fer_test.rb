1090dfdd045526587e5a78012da7107628354d4e
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-01-12 21:20:07 +0000

    Exploring other string formatting options

--- ruby/two-fer/two_fer_test.rb
+++ ruby/two-fer/two_fer_test.rb
@@ -5,13 +5,22 @@ require_relative 'two_fer'
 class TwoFerTest < Minitest::Test
   def test_no_name_given
     assert_equal "One for you, one for me.", TwoFer.two_fer
+    assert_equal "One for you, one for me.", TwoFer.two_fer_note
+    assert_equal "One for you, one for me.", TwoFer.two_fer_sprintf
+    assert_equal "One for you, one for me.", TwoFer.two_fer_interpolation
   end
 
   def test_a_name_given
     assert_equal "One for Alice, one for me.", TwoFer.two_fer("Alice")
+    assert_equal "One for Alice, one for me.", TwoFer.two_fer_note("Alice")
+    assert_equal "One for Alice, one for me.", TwoFer.two_fer_sprintf("Alice")
+    assert_equal "One for Alice, one for me.", TwoFer.two_fer_interpolation("Alice")
   end
 
   def test_another_name_given
     assert_equal "One for Bob, one for me.", TwoFer.two_fer("Bob")
+    assert_equal "One for Bob, one for me.", TwoFer.two_fer_note("Bob")
+    assert_equal "One for Bob, one for me.", TwoFer.two_fer_sprintf("Bob")
+    assert_equal "One for Bob, one for me.", TwoFer.two_fer_interpolation("Bob")
   end
 end

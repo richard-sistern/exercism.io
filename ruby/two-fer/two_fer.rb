23f45a136ce8ad875492dca71e76b16fbbb65969
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-01-13 10:10:33 +0000

    sprintf branch

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,20 +1,6 @@
 class TwoFer
-  def self.two_fer(name = 'you')
-    'One for %s, one for me.' % name
-  end
-
-  # With an annotation reference
-  def self.two_fer_note(name = 'you')
-    'One for %<name>s, one for me.' % { name: name }
-  end
-
   # Using full sptringf syntax
-  def self.two_fer_sprintf(name = 'you')
+  def self.two_fer(name = 'you')
     sprintf('One for %<name>s, one for me.', { :name => name })
-  end
-
-  # Using interpolation
-  def self.two_fer_interpolation(name = 'you')
-    "One for #{name}, one for me."
   end
 end

217aae2790f270b1d5716d8ef13a56a11fec1ed9
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-01-14 15:16:23 +0000

    Gigasecond task passing all tests

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,8 +1,8 @@
-=begin
-Write your code for the 'Gigasecond' exercise in this file. Make the tests in
-`gigasecond_test.rb` pass.
+# Calculate the moment when someone has lived for 10^9 seconds.
+# A gigasecond is 10^9 (1,000,000,000) seconds.
 
-To get started with TDD, see the `README.md` file in your
-`ruby/gigasecond` directory.
-=end
-
+class Gigasecond
+  def self.from(dob)
+    dob + 1000000000
+  end
+end

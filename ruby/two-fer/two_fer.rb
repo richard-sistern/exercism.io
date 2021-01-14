98bbbb7188cc3dccdb832523709f980af548e5f4
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-01-14 14:04:17 +0000

    Improving with mentor feedback

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,5 +1,22 @@
+# Incorporating improvements from mentor feedback
+
 class TwoFer
-  def self.two_fer(name = 'you')
-    'One for %s, one for me.' % name
+  TWO_FER =
+    {
+      English: 'One for %<name>s, one for me.',
+      Spanish: 'Uno para el %<name>s, uno para mí.',
+      German:  'Einer für den %<name>s, einer für mich.',
+      Dutch:   'Eén voor %<name>s, één voor mij.'
+    }
+
+  def self.two_fer(name = 'you', language = :English)
+    TWO_FER[language] % { name: name }
   end
+end
+
+if $PROGRAM_NAME == __FILE__
+  puts TwoFer.two_fer
+  puts TwoFer.two_fer 'Jorge', :Spanish
+  puts TwoFer.two_fer 'Bjorn', :Dutch
+  puts TwoFer.two_fer 'George'
 end
--- ruby/two-fer/two_fer_test.rb
+++ ruby/two-fer/two_fer_test.rb
@@ -14,4 +14,12 @@ class TwoFerTest < Minitest::Test
   def test_another_name_given
     assert_equal "One for Bob, one for me.", TwoFer.two_fer("Bob")
   end
+
+  def test_a_spanish_name_given
+    assert_equal "Uno para el Jorge, uno para mí.", TwoFer.two_fer('Jorge', :Spanish)
+  end
+
+  def test_a_dutch_name_given
+    assert_equal "Eén voor Bjorn, één voor mij.", TwoFer.two_fer('Bjorn', :Dutch)
+  end
 end

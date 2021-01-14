e5d1d5cfe01097f41db8aba22ec86f5d2be69f69
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-01-14 15:39:15 +0000

    Changed gigasecond from 1000000000 to 10**9

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -2,6 +2,6 @@ class Gigasecond
   # Calculate the moment when someone has lived for 10^9 seconds.
   def self.from(dob)
     # A gigasecond is 10^9 (1,000,000,000) seconds.
-    dob + 1000000000
+    dob + 10**9
   end
 end

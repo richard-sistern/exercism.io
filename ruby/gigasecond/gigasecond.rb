09e5232bc38a8c06c9ab296e172992c60b932ea1
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-01-14 15:34:32 +0000

    Tidying comments

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,8 +1,7 @@
-# Calculate the moment when someone has lived for 10^9 seconds.
-# A gigasecond is 10^9 (1,000,000,000) seconds.
-
 class Gigasecond
+  # Calculate the moment when someone has lived for 10^9 seconds.
   def self.from(dob)
+    # A gigasecond is 10^9 (1,000,000,000) seconds.
     dob + 1000000000
   end
 end

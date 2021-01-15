1fdca19d6e1a0f302787a67cc69517330d7ebfb1
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-01-15 13:12:00 +0000

    Resistor color task done

--- ruby/resistor-color/resistor_color.rb
+++ ruby/resistor-color/resistor_color.rb
@@ -1,8 +1,12 @@
-=begin
-Write your code for the 'Resistor Color' exercise in this file. Make the tests in
-`resistor_color_test.rb` pass.
+class ResistorColor
+  COLORS = %w(black brown red orange yellow green blue violet grey white)
 
-To get started with TDD, see the `README.md` file in your
-`ruby/resistor-color` directory.
-=end
+  def self.color_code(color)
+    COLORS.index(color)
+  end
+end
 
+if $PROGRAM_NAME == __FILE__
+  puts ResistorColor.color_code('black')
+  puts ResistorColor.color_code('blue')
+end

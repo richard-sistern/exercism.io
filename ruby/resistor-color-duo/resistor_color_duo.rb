299ff395a20980214aa31c1b711d723ef9ed2f5b
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-01-15 15:20:18 +0000

    Resistor Color Duo task done

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,5 +1,15 @@
 class ResistorColorDuo
-  def self.value(colours)
+  BANDS = %w(black brown red orange yellow green blue violet grey white)
 
+  def self.value(colors)
+    colors[0..1]
+      .map {|color| BANDS.index(color)}
+      .join
+      .to_i
   end
+end
+
+if $PROGRAM_NAME == __FILE__
+  puts ResistorColorDuo.value(["orange", "orange"])
+  puts ResistorColorDuo.value(["green", "brown", "orange"])
 end

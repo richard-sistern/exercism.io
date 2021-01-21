f1391ad5915dfcb17d115e5cf1a19e035cbc81a5
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-01-21 14:50:02 +0000

    Microwave task complete

--- ruby/microwave/microwave.rb
+++ ruby/microwave/microwave.rb
@@ -1,7 +1,17 @@
-=begin
-Write your code for the 'Microwave' exercise in this file. Make the tests in
-`microwave_test.rb` pass.
+class Microwave
+  def initialize(time)
+    @minutes, @seconds = time.divmod(100)
+    overflow, @seconds = @seconds.divmod(60)
+    @minutes += overflow
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/microwave` directory.
-=end
+  def timer
+    "%<minutes>.2d:%<seconds>.2d" % [minutes: @minutes, seconds: @seconds]
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  p Microwave.new(59).timer
+  p Microwave.new(272).timer
+  p Microwave.new(1001).timer
+end

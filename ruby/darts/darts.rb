952b06e806d471ec8db84052af86e524c3d727e1
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-01-21 15:55:38 +0000

    Darts task complete

--- ruby/darts/darts.rb
+++ ruby/darts/darts.rb
@@ -1,7 +1,20 @@
-=begin
-Write your code for the 'Darts' exercise in this file. Make the tests in
-`darts_test.rb` pass.
+class Darts
+    def initialize(x, y)
+      @x, @y = x, y
+    end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/darts` directory.
-=end
+    def score
+      "#{@x} : #{@y}"
+      case Math.hypot(@x, @y)
+        when 0..1 then 10
+        when 1..5 then 5
+        when 5..10 then 1
+        else 0
+      end
+    end
+end
+
+if $PROGRAM_NAME == __FILE__
+  darts = Darts.new(-9, 9)
+  p darts.score
+end

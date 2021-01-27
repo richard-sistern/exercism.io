a991243fe8d036d07a447cf88aea8cc1981e640a
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-01-27 16:02:27 +0000

    Reworked after mentor feedback

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,10 +1,21 @@
 class ResistorColorDuo
-  BANDS = %w(black brown red orange yellow green blue violet grey white)
+  BANDS =
+    {
+      black: '0',
+      brown: '1',
+      red: '2',
+      orange: '3',
+      yellow: '4',
+      green: '5',
+      blue: '6',
+      violet: '7',
+      grey: '8',
+      white: '9'
+    }
 
   def self.value(colors)
-    colors[0..1]
-      .map {|color| BANDS.index(color)}
-      .join
+    colors.take(2)
+      .sum('') {|color| BANDS[color.to_sym]}
       .to_i
   end
 end

3281bb18422287f3f931d22353b79547457a3311
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-01-28 10:27:24 +0000

    Added Acronym task

---
+++ ruby/acronym/acronym.rb
@@ -0,0 +1,9 @@
+=begin
+Write your code for the 'Acronym' exercise in this file. Make the tests in
+`acronym_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/acronym` directory.
+=end
+
+#https://ivoanjo.me/blog/2017/09/20/why-i-always-use-attr_reader-to-access-instance-variables/

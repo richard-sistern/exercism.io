d9309f0467a60cf244c7951a7726f320cde284d6
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-01-28 10:28:19 +0000

    Improved text alignment for BANDS hash

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,16 +1,16 @@
 class ResistorColorDuo
   BANDS =
     {
-       black: 0,
-       brown: 1,
-         red: 2,
+      black:  0,
+      brown:  1,
+      red:    2,
       orange: 3,
       yellow: 4,
-       green: 5,
-        blue: 6,
+      green:  5,
+      blue:   6,
       violet: 7,
-        grey: 8,
-        white: 9
+      grey:   8,
+      white:  9
     }
 
   def self.value(colors)

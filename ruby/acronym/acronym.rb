b33531c765e937a9b2d611c2b5bceeae21257ef9
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-01-29 08:15:55 +0000

    Acronym tests passing

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,9 +1,31 @@
+# TODO: Reorder code
+# TODO: Clean comments
+
+class Acronym
+  attr_reader :acronym
+
+  def self.abbreviate(phrase)
+    new(phrase).acronym
+  end
+
+  private
+
+  def initialize(phrase)
+    @acronym = phrase.scan(/\b\w/).join.upcase
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  puts Acronym.abbreviate('Portable Network Graphics')
+end
+
+# https://ivoanjo.me/blog/2017/09/20/why-i-always-use-attr_reader-to-access-instance-variables/
+# https://stackoverflow.com/questions/46863457/how-to-select-first-letter-of-each-word-of-a-string-using-regex
+# https://www.rubyguides.com/2015/06/ruby-regex/
+
 =begin
-Write your code for the 'Acronym' exercise in this file. Make the tests in
-`acronym_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/acronym` directory.
+irb(main):001:0> "Portable Network Graphics".scan(/\b\w/)
+=> ["P", "N", "G"]
+irb(main):002:0> "Portable Network Graphics".scan(/\b\w/).join
+=> "PNG"
 =end
-
-#https://ivoanjo.me/blog/2017/09/20/why-i-always-use-attr_reader-to-access-instance-variables/

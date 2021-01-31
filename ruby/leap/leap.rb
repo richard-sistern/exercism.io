72126d34f9f2c04720dc1ce5ad505025f50857fe
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-01-31 22:12:58 +0000

    Initial leap year script.  All tests passing.

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,8 +1,19 @@
-=begin
-Write your code for the 'Leap' exercise in this file. Make the tests in
-`leap_test.rb` pass.
+class Year
+  # TODO: Refactor
 
-To get started with TDD, see the `README.md` file in your
-`ruby/leap` directory.
-=end
+  def self.leap?(year)
+    # on every year that is evenly divisible by 4
+    # except every year that is evenly divisible by 100
+    # unless the year is also evenly divisible by 400
 
+    year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
+
+    # % (modulo) gives the remaining of a division
+    # https://www.rubyguides.com/2018/07/ruby-operators/
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  puts Year.leap?(2015) # false
+  puts Year.leap?(1996) # true
+end

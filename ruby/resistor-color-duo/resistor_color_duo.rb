f5333751c68009557b115e04cbcc5f3007e1fe2a
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-01-31 18:21:14 +0000

    Remove comments

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,11 +1,4 @@
 class ResistorColorDuo
-  # TODO: Reorder code - PENDING REVIEW
-  # TODO: Rework using each_with_object - D
-  # TODO: Review freeze again
-  # TODO: take(2) in class method or initialize?
-  # TODO: is overloading to_i good or bad?
-  # TODO: Define why Interger(string) is better
-
   BANDS =
     {
       black:  0,
@@ -43,16 +36,3 @@ if $PROGRAM_NAME == __FILE__
   puts ResistorColorDuo.value %w'orange orange'
   puts ResistorColorDuo.value %w'green brown orange'
 end
-
-# .LINKS
-#
-# https://www.peterdebelak.com/blog/private-attr-readers/
-# https://womanonrails.com/each-with-object
-# https://apidock.com/rails/Enumerable/each_with_object
-# https://naturaily.com/blog/public-private-protected-ruby
-# https://medium.com/@leizl.samano/how-to-use-each-with-object-in-ruby-dccca07bfa18
-# https://bigbinary.com/blog/ruby-2-6-adds-option-to-not-raise-exception-for-integer-float-methods
-# https://ruby-doc.com/docs/ProgrammingRuby/html/tut_classes.html
-# https://www.toptal.com/ruby/ruby-metaprogramming-cooler-than-it-sounds
-# https://www.reddit.com/r/ruby/comments/9j0ooc/freeze_your_constants/
-# https://www.rubypigeon.com/posts/a-review-of-immutability-in-ruby/

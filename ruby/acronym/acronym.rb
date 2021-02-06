ae40da4347da52df5cd67eff43aa9ea04aa9cec9
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-02-06 19:51:39 +0000

    Tidied code and flow to submit to exercism

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,31 +1,23 @@
 class Acronym
-  attr_reader :acronym
-
   def self.abbreviate(phrase)
-    new(phrase).acronym
+    new(phrase).upcase
   end
 
   private
 
   def initialize(phrase)
-    @acronym = phrase.scan(/\b\w/).join.upcase
+    first_char_of_each_word = Regex.new('/\b\w/')
+    @acronym = phrase.scan(first_char_of_each_word).join
+  end
+
+  public
+
+  def upcase
+    @acronym.upcase
   end
 end
 
 if $PROGRAM_NAME == __FILE__
   puts Acronym.abbreviate('Portable Network Graphics')
+  puts Acronym.abbreviate('Something - I made up from thin air')
 end
-
-# TODO: Reorder code
-# TODO: Clean comments
-
-# https://ivoanjo.me/blog/2017/09/20/why-i-always-use-attr_reader-to-access-instance-variables/
-# https://stackoverflow.com/questions/46863457/how-to-select-first-letter-of-each-word-of-a-string-using-regex
-# https://www.rubyguides.com/2015/06/ruby-regex/
-
-=begin
-irb(main):001:0> "Portable Network Graphics".scan(/\b\w/)
-=> ["P", "N", "G"]
-irb(main):002:0> "Portable Network Graphics".scan(/\b\w/).join
-=> "PNG"
-=end

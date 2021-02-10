f1892cd8561b84664b1eafb8ded9bb62e11fdd31
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-02-10 09:48:09 +0000

    Leap task finished

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,15 +1,16 @@
 class Year
-  # TODO: Refactor
-
   def self.leap?(year)
     # on every year that is evenly divisible by 4
     # except every year that is evenly divisible by 100
     # unless the year is also evenly divisible by 400
 
-    (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+    ((year % 4).zero? && year % 100 != 0) || (year % 400).zero?
 
     # % (modulo) gives the remaining of a division
     # https://www.rubyguides.com/2018/07/ruby-operators/
+
+    # Tell, don't ask
+    # https://www.reddit.com/r/ruby/comments/50qns8/zero_instead_of_0/
   end
 end
 

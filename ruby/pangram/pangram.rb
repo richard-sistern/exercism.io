b3b7620cce371f7e2d7f29229b26073180dde436
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-02-10 10:52:16 +0000

    Finished Pangram task

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,10 +1,18 @@
 class Pangram
+  ALPHABET = ('a'..'z')
+
   def self.pangram?(sentence)
-    sentence
+    sentence.downcase!
+    ALPHABET.all? { |letter| sentence.include? letter }
+
+    # all?(*args)
+    # Passes each element of the collection to the given block.
+    # The method returns true if the block never returns false or nil.
+    # https://apidock.com/ruby/Enumerable/all%3F
   end
 end
 
 if $PROGRAM_NAME == __FILE__
-  puts Pangram.pangram?('abcdefghijklmnopqrstuvwxyz') # true
+  puts Pangram.pangram?('abcdefghijklmnopqrstuvwxyZ') # true
   puts Pangram.pangram?('a quick movement of the enemy will jeopardize five gunboats') # false
 end

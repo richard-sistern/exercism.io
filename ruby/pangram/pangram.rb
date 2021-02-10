9e3dbb9811a4d93ff6cc6cb6925bfe957313f219
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-02-10 09:56:13 +0000

    Pangram scaffold

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,8 +1,10 @@
-=begin
-Write your code for the 'Pangram' exercise in this file. Make the tests in
-`pangram_test.rb` pass.
+class Pangram
+  def self.pangram?(sentence)
+    sentence
+  end
+end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/pangram` directory.
-=end
-
+if $PROGRAM_NAME == __FILE__
+  puts Pangram.pangram?('abcdefghijklmnopqrstuvwxyz') # true
+  puts Pangram.pangram?('a quick movement of the enemy will jeopardize five gunboats') # false
+end

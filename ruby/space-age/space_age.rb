2824d3dbd40ca155f483aa5f4acbf464b0fe3710
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-02-18 21:48:43 +0000

    completes space age task with all tests passing

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,8 +1,32 @@
-=begin
-Write your code for the 'Space Age' exercise in this file. Make the tests in
-`space_age_test.rb` pass.
+class SpaceAge
+  SECONDS_IN_EARTH_YEAR = 31_557_600
 
-To get started with TDD, see the `README.md` file in your
-`ruby/space-age` directory.
-=end
+  ORBITAL_PERIODS_IN_EARTH_YEARS = {
+    earth:    1,
+    mercury:  0.2408467,
+    venus:    0.61519726,
+    mars:     1.8808158,
+    jupiter:  11.862615,
+    saturn:   29.447498,
+    uranus:   84.016846,
+    neptune:  164.79132
+  }
 
+  private
+
+  def initialize(seconds)
+    @seconds = seconds.to_f
+  end
+
+  public
+
+  ORBITAL_PERIODS_IN_EARTH_YEARS.each do |planet, orbital_period|
+    define_method("on_#{planet}".to_sym) do
+      @seconds / (SECONDS_IN_EARTH_YEAR * orbital_period)
+    end
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  puts SpaceAge.new(1_000_000_000).on_earth # ~31.69
+end

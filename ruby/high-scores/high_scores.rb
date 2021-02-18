a2f04c4a3d2d52abeacfbe3bb5acca8ecbc4d554
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-02-18 20:33:45 +0000

    completes high scores task with all tests passing

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -1,8 +1,19 @@
-=begin
-Write your code for the 'High Scores' exercise in this file. Make the tests in
-`high_scores_test.rb` pass.
+class HighScores
 
-To get started with TDD, see the `README.md` file in your
-`ruby/high-scores` directory.
-=end
+  private
 
+  def initialize(scores)
+     @scores = scores
+     @latest = scores.last
+     @personal_best = scores.max
+     @personal_top_three = scores.max(3)
+  end
+
+  public
+
+  def latest_is_personal_best?
+    @latest == @personal_best
+  end
+
+  attr_reader :scores, :latest, :personal_best, :personal_top_three
+end

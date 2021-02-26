3295ab2f5a458a138d01fc48723109ccde172700
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-02-26 15:08:43 +0000

    rework two fer to use f-string formatting

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,5 +1,2 @@
-from string import Template
-t = Template("One for $name, one for me.")
-
 def two_fer(name = 'you'):
-    return(t.substitute(name=name))
+    return(f"One for {name}, one for me.")

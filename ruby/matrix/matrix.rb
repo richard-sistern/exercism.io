f01482bda708c7529c5e730e2511d83bb077f665
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-03-08 21:03:57 +0000

    add initial Matrix solution

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,8 +1,13 @@
-=begin
-Write your code for the 'Matrix' exercise in this file. Make the tests in
-`matrix_test.rb` pass.
+class Matrix
+    def initialize(lines)
+        @lines = lines
+    end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/matrix` directory.
-=end
+    def rows
+        @lines.each_line.map { |line| line.split.map &:to_i }
+    end
 
+    def columns
+        rows.transpose
+    end
+end

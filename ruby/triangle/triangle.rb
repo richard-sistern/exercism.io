8ad83e0fc06981070c055c2e9f621a4cdaee5994
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-03-10 09:33:34 +0000

    remove comments and tidy code

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -1,9 +1,6 @@
 class Triangle
   attr_reader :sides, :unique_sides, :sum_sides
 
-  # DONE: Check for actual triangle 
-  # TODO: Degenerate
-  # TODO: Tidy up!!!
   def initialize(sides)
     @sides = sides.sort
     @unique_sides = sides.uniq.count
@@ -15,7 +12,6 @@ class Triangle
   end
 
   def isosceles?
-    # DONE: Add a check for equilateral?
     unique_sides == 2 || equilateral? if triangle?
   end
 
@@ -24,33 +20,14 @@ class Triangle
   end
 
   def degenerate?
-    #triangle?
-    #sides.sort!
-    sides[0] + sides[1] <= sides[2] && sum_sides > 0
-    # sort asc
-    # a+b<=c
+    sides[0] + sides[1] <= sides[2] && sum_sides.positive?
   end
 
   private
 
   def triangle?
-    # DONE: Rework to remove !
-    # the sum of the lengths of any two sides must be greater than or 
-    # equal to the length of the third side
-    #sum_sides = sides.sum
-    #sides.none? { |side| (sides.sum - side) <= side }
     sides.all? { |side| side < sum_sides - side }
   end
-end
-
-if $PROGRAM_NAME == __FILE__
-  # test_equilateral_triangles_are_also_isosceles
-  triangle = Triangle.new([4, 4, 4])
-  puts triangle.isosceles? # true
-
-  # test_triangle_is_not_isosceles_if_no_sides_are_equal
-  triangle = Triangle.new([2, 3, 4])
-  puts triangle.isosceles? # false
 end
 
 if defined?(Minitest)
@@ -76,5 +53,3 @@ if defined?(Minitest)
     end
   end
 end
-
- 

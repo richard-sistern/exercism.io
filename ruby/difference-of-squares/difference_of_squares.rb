76c674f5aad83914cb763b3bf510c693d2c81e86
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-04-07 13:36:39 +0100

    Finish Difference of Squares task

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -4,17 +4,22 @@ class Squares
     end
 
     def square_of_sum
+        @values.reduce(:+) ** 2
     end
 
     def sum_of_squares
+        @values
+            .map { |v| v ** 2 }
+            .sum
     end
 
     def difference
+        square_of_sum - sum_of_squares
     end
 end
 
 if $PROGRAM_NAME == __FILE__
-  a =  Squares.new(100)    
-  puts a.inspect
-  puts a.instance_of? Array
+  puts Squares.new(1).square_of_sum # 1
+  puts Squares.new(1).sum_of_squares # 1
+  puts Squares.new(5).difference # 170
 end

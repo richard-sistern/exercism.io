a9765595eb4b4e82b418660c72a86ba2aa356d9d
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-05-02 21:05:51 +0100

    Add scaffold for Series task

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,8 +1,8 @@
-=begin
-Write your code for the 'Series' exercise in this file. Make the tests in
-`series_test.rb` pass.
+class Series
+    def initialize(series)
+        @series = series
+    end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/series` directory.
-=end
-
+    def slices(number)
+    end
+end

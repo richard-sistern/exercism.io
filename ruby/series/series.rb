2352b05131b196dd0c0dd3fbbff2d60852b492be
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-05-03 18:52:49 +0100

    Complete Series task with all tests passing

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -4,5 +4,14 @@ class Series
     end
 
     def slices(number)
+        raise ArgumentError if number > @series.length
+
+        @series.chars.each_cons(number).map(&:join)
     end
+end
+
+if $PROGRAM_NAME == __FILE__
+    series = Series.new('01234')
+    puts series.slices(1) # ['0', '1', '2', '3', '4']
+    puts series.slices(2) # ['01', '12', '23', '34']
 end
